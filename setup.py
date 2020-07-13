03b46ceb2902b211e7c9ac2f0e295bc6f8b792de
unknown <nadav.tsa@gmail.com>
2020-07-13 13:28:52 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='my_package',
+    url='https://github.com/nadavtsa/my_package',
+    author='nadav',
+    author_email='nadav.tsa@gmail.com',
+    packages=['my_package'],
+    long_description=open('README.md').read()
+)
+
+# .Values.elasticsearchUrl
