0c0d3d70716947543aef7371a7892c6a3bd2956a
unknown <nadav.tsa@gmail.com>
2020-07-19 08:36:48 +0300

    first commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/nadavtsa/my_package',
     author='nadav',
     author_email='nadav.tsa@gmail.com',
-    packages=['my_package'],
+    packages=['my_package', 'bye'],
     long_description=open('README.md').read()
 )
 
